afe4aa3253eadd05e34b7e4f90a2a53751126b6d
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-09-24 10:22:59 -0300

    refactor: change code language to English

---
+++ library-management/models.py
@@ -0,0 +1,23 @@
+class Book():
+    def __init__(self, title, author, year, genre, code) -> None:
+        self.title = title
+        self.author = author
+        self.year = year
+        self.genre = genre
+        self.code = code
+    
+    def show_info():
+        #TODO
+        pass
+    
+
+class Usuario():
+    def __init__(self, name, email, phone, user_code) -> None:
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.user_code = user_code
+
+    def show_info():
+        #TODO
+        pass
