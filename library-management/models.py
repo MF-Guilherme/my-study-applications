54e9a81e3151a038411703c536aeb467b6684d43
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-09-24 10:35:03 -0300

    feat: show info methods from models

--- library-management/models.py
+++ library-management/models.py
@@ -6,18 +6,16 @@ class Book():
         self.genre = genre
         self.code = code
     
-    def show_info():
-        #TODO
-        pass
-    
+    def show_info(self):
+        return f'Title: {self.title} | Author: {self.author} | Publication Year: {self.year} | Genre: {self.genre} | ISBN code: {self.code}'
+            
 
-class Usuario():
+class User():
     def __init__(self, name, email, phone, user_code) -> None:
         self.name = name
         self.email = email
         self.phone = phone
         self.user_code = user_code
 
-    def show_info():
-        #TODO
-        pass
+    def show_info(self):
+        return f'Name: {self.name} | E-mail: {self.email} | Phone: {self.phone} | User code: {self.user_code}'
