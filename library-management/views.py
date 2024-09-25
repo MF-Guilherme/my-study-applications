73ce9790d3bc3bbf2008f479d124529f74352798
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-09-25 11:21:41 -0300

    feat(view): menu, book register and show books implemented

---
+++ library-management/views.py
@@ -0,0 +1,52 @@
+from controllers import BookController, UserController
+
+book_controller = BookController()
+
+def show_menu():
+    ipt = input(f'- Type "book" to manage books.\n'
+                f'- Type "user" to manage users.\n'
+                f'{'-' * 50}\n')
+    if ipt == 'book' or ipt == 'books' or ipt == 'Book' or ipt == 'Books':
+        print(f'- To add a new book enter 1.\n'
+              f'- To list all books enter 2.\n'
+              f'- To search for a book enter 3.\n'
+              f'- To update a book enter 4.\n'
+              f'- To delete a book enter 5.')
+        print('-' * 50)
+    elif ipt == 'user' or ipt == 'users' or ipt == 'User' or ipt == 'Users':
+        print(f'- To add a new user enter 1.\n'
+              f'- To list all users enter 2.\n'
+              f'- To search for a user enter 3.\n'
+              f'- To update a user enter 4.\n'
+              f'- To delete a user enter 5')
+        print('-' * 50)
+    else:
+        print('Invalid input! Please, enter "book" or "user".')
+        print('-' * 50)
+    return ipt
+
+def get_book_infos():
+    title = input("Book title: ")
+    author = input("Author: ")
+    year = input("Publication year: ")
+    genre = input("Literary genre: ")
+    code = input("ISBN code: ")
+    return title, author, year, genre, code
+
+def book_register(controller):
+    title, author, year, genre, code = get_book_infos()
+    controller.add_book(title, author, year, genre, code)
+    print('Book registered!')
+
+def show_books(controller):
+    books = controller.list_books()
+    if books:
+        for book in books:
+            print(f'Title: {book.title} | Author: {book.author}')
+    else: 
+        print('There are no books registered yet')
+
+
+if __name__ == "__main__":
+    book_register(book_controller)
+    show_books(book_controller)
