1ec7f36234b60ea0870e57947065c89ec4d86ac5
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-09-27 18:23:10 -0300

    feat(views): user views implemented

--- library-management/views.py
+++ library-management/views.py
@@ -2,10 +2,14 @@ from controllers import BookController, UserController
 from prompt_toolkit import prompt
 
 book_controller = BookController()
+user_controller = UserController()
+
+# BOOK VIEWS
 
 def show_menu():
     ipt = input(f'- Type "book" to manage books.\n'
                 f'- Type "user" to manage users.\n'
+                f'- Type 0 "zero" to exit system.\n'
                 f'{'-' * 50}\n')
     if ipt.lower() in ['book', 'books']:
         print(f'- To add a new book enter 1.\n'
@@ -72,14 +76,52 @@ def update_book(controller):
         controller.update_book(ipt_code, new_title, new_author, new_year, new_genre)
         print('Book updated')
     else:
-        print("Book not find")
+        print("Book not found")
 
+# USER VIEWS
+    
+def get_user_infos():
+    name = input("Name: ")
+    email = input("E-mail: ")
+    phone = input("Phone number: ")
+    user_code = input("User code: ")
+    return name, email, phone, user_code
 
-if __name__ == "__main__":
-    show_menu()
-    book_register(book_controller)
-    print('-' * 50)
-    show_books(book_controller)
-    print('-' * 50)
-    update_book(book_controller)
-    show_books(book_controller)
+def user_register(controller):
+    name, email, phone, user_code = get_user_infos()
+    controller.register_user(name, email, phone, user_code)
+    print('User registered!')
+
+def show_users(controller):
+    users = controller.list_users()
+    if users:
+        for user in users:
+            print(f'Name: {user.name} | Email: {user.email} | User code: {user.user_code}')
+    else: 
+        print('There are no users registered yet')
+
+def search_user(controller):
+    ipt_code = input('Enter the user code: ')
+    user = controller.find_by_user_code(ipt_code)
+    if user:
+        print(f' User code: {user.user_code} | Name: {user.name} | Email: {user.email} | Phone number: {user.phone}')
+    else:
+        print('User not found')
+    
+def delete_user(controller):
+    ipt_code = input('Enter the user code you want to delete: ')
+    if not controller.delete_user(ipt_code):
+        print("User not found.")
+    else: print("User deleted.")
+
+def update_user(controller):
+    ipt_code = input('Enter the user code you want to update: ')
+    user = controller.find_by_user_code(ipt_code)
+    if user:
+        new_name = prompt("Name: ", default=user.name).strip() or user.name
+        new_email = prompt("E-mail: ", default=user.email).strip() or user.email
+        new_phone = prompt("Phone number: ", default=user.phone).strip() or user.phone
+        controller.update_user(ipt_code, new_name, new_email, new_phone)
+        print('User updated')
+    else:
+        print("User not found")
